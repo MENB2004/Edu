674ca35e606676e9dff0811e89a2b5a88b7d5a4c
MENB2004 <nb2004416@gmail.com>
2026-02-09 15:25:23 +0530

    course details updated

--- projango/campus_core/courses/urls.py
+++ projango/campus_core/courses/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('catalog/', views.course_catalog),
-    path('details/',views.course_details),
+    path('catalog/', views.course_form),
+    path('details/',views.course_details,name='coursedetails'),
+    path('delete/<int:sid>',views.course_delete),
+    path('update/<int:sid>',views.course_update),
 ]
--- projango/campus_core/courses/views.py
+++ projango/campus_core/courses/views.py
@@ -1,12 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Course
+from .forms import CourseForm
 # Create your views here.
-def course_catalog(request):
-    courses=['Btech Cse','Btech ECE', 'Btech CE','Btech EE','Mtech Cyber Security','Mtech Embedded System']
-    count={"UG": 4,"PG": 2}
-    return render(request, 'courses/course_catalog.html',{'courses': courses,'count':count})
+def course_form(request):
+    if request.method=="POST":
+        form=CourseForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            form.save()
+        return redirect('coursedetails')
+    else:
+        form=CourseForm()
+        return render(request, 'courses/course_form.html',{'form':form})
 
 def course_details(request):
     details=Course.objects.all()
+    print(details)
     return render(request, 'courses/course_details.html',{'details':details})
+
+def course_delete(request,sid):
+    record=Course.objects.get(id=sid)
+    record.delete()
+    return redirect('coursedetails')
+
+def course_update(request,sid):
+    record=Course.objects.get(id=sid)
+    if request.method=="GET":
+        form=CourseForm(instance = record)
+        return render(request,'courses/update_form.html',{'form':form})
+    else:
+        form=CourseForm(request.POST, instance=record)
+        if form.is_valid():
+            print(form.cleaned_data)
+            form.save()
+        return redirect('coursedetails')
+# Create your views here.
